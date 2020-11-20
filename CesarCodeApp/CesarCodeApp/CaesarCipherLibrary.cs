107d61ef951807b6d06914b587221be89da867b2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 11:12:55 +0100

    Ho inserito delle classi e scritto il codice per la codifica e la decodifica

---
+++ CesarCodeApp/CesarCodeApp/CaesarCipherLibrary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipherLibrary
+{
+    public abstract class CaesarCipher
+    {
+        protected int Key { get; set; }
+        private char Cipher(char ch)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+            char offset;
+            if(char.IsUpper(ch))
+            {
+                offset = 'A';
+            }
+            else
+            {
+                offset = 'a';
+            }
+            return (char)(((ch - offset + Key) % 26) + offset);
+        }
+        protected string DoWork(string m)
+        {
+            string message = string.Empty;
+            foreach (char ch in m)
+                message += Cipher(ch);
+            return message;
+        }
+    }
+}
