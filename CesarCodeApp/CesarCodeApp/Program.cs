bfe39e128180378230f5058f6759a43373bf8c76
Martyb13 <45844247+Martyb13@users.noreply.github.com>
2020-11-27 21:01:25 +0100

    Ora il codice è in grado di criptare una parola

--- CesarCodeApp/CesarCodeApp/Program.cs
+++ CesarCodeApp/CesarCodeApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CaesarCipherLibrary;
 
 namespace CesarCodeApp
 {
@@ -6,7 +7,27 @@ namespace CesarCodeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci una parola:");
+            string plaintext = Console.ReadLine();
+            int key;
+            CaesarCode ml;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Inserisci la chiave");
+                    key = int.Parse(Console.ReadLine());
+                } while (key<0||key>25);
+
+                ml = new CaesarCode(plaintext, key);
+                Console.WriteLine($"Il testo cifrato è: {ml.CipherText}");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
